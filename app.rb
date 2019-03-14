24af8c037413382a2c9caead3a0a08b756761b51
Brandon Teich <brandonteich123@gmail.com>
2019-03-14 20:25:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+<<<<<<< HEAD
   get '/' do
     "Hello, World!"
   end
     
+=======
+  
+
+>>>>>>> d61fe1c7475e460120af66e1b328dcf6e863a4e5
 end
